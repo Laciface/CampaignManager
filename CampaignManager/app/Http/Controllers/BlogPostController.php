23b1e59485aa7c9ef75ab6f5bfdc5266a3547765
Laciface <csomali18@gmail.com>
2021-05-10 13:09:45 +0200

    feature: insert Post, and display form done

---
+++ CampaignManager/app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BlogPostController extends Controller
+{
+    public function displayBlogPostForm(){
+        return view('addBlogPost');
+    }
+
+    public function createPost(Request $request){
+        $title = $request->input('title');
+        $post = $request->input('post');
+        $time = $request->added_at;
+
+        $arrayToInsert = array('title'=> $title, 'post' => $post, 'added_at' => $time);
+        DB::table('blog_posts')->insert($arrayToInsert);
+    }
+}
