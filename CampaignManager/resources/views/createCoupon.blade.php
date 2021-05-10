97bf4c947462cb55cec615274bde4a92320dda07
Laciface <csomali18@gmail.com>
2021-05-10 13:18:06 +0200

    refactor: select tag provided

--- CampaignManager/resources/views/createCoupon.blade.php
+++ CampaignManager/resources/views/createCoupon.blade.php
@@ -5,8 +5,17 @@
     <div>
         <form action="" method="post" enctype="multipart/form-data">
             {{ csrf_field() }}
-            <label for="discount">Kedvezmény mértéke (%ban)</label>
-            <input type="number" name="discount" required>
+            <label for="name">Kupon neve</label>
+            <input type="text" name="name" required>
+            <label for="discount">Kedvezmény mértéke</label>
+            <select name="discount" required>
+                <option value="0.05">5%</option>
+                <option value="0.1">10%</option>
+                <option value="0.15">15%</option>
+                <option value="0.2">20%</option>
+                <option value="0.25">25%</option>
+                <option value="0.3">30%</option>
+            </select>
             <button type="submit">Létrehoz</button>
         </form>
     </div>
