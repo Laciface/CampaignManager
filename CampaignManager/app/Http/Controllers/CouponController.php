bbe3023fe27069b8a04226a2e3ca2a226654af0d
Laciface <csomali18@gmail.com>
2021-05-11 18:42:51 +0200

    feature: addCouponToCampaign implemented

--- CampaignManager/app/Http/Controllers/CouponController.php
+++ CampaignManager/app/Http/Controllers/CouponController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Campaign;
+use App\Models\Coupon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -20,5 +22,21 @@ class CouponController extends Controller
         DB::table('coupons')->insert($arrayToInsert);
 
         return redirect()->route('displayCampaigns');
+    }
+
+    public function addCouponToCampaign(Request $request, $id){
+        $couponId = $request->input('couponId');
+        $couponIdList = Coupon::where('id', $id)->value('coupons');
+        $msg = "ez a kupon már hozzá lett adva a kampányhoz";
+        $this->checkTheExistence($couponId, $couponIdList, $msg, $id);
+        if($couponIdList == null){
+            DB::table('campaigns')->where('id', $id)->update(['coupons'=> array($couponId)]);
+        } else {
+            array_push($couponIdList, $couponId);
+            DB::table('campaigns')->where('id', $id)->update(['coupons'=> $couponIdList]);
+        }
+
+        header("Location: http://localhost:8000/campaignHandler/$id", true);
+        die();
     }
 }
