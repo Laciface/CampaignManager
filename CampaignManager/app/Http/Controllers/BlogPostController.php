f8d594b8e17d8ad7c5431fb630263db72ed1c78b
Laciface <csomali18@gmail.com>
2021-05-11 18:22:24 +0200

    feature: addPostToCampaign implemented

--- CampaignManager/app/Http/Controllers/BlogPostController.php
+++ CampaignManager/app/Http/Controllers/BlogPostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BlogPost;
+use App\Models\Campaign;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -21,5 +22,21 @@ class BlogPostController extends Controller
         DB::table('blog_posts')->insert($arrayToInsert);
 
         return redirect()->route('displayCampaigns');
+    }
+
+    public function addPostToCampaign(Request $request, $id){
+        $postId = $request->input('postId');
+        $postIdList = Campaign::where('id', $id)->value('posts');
+        $msg = "ez a bejegyzés már hozzá lett adva a kampányhoz";
+        $this->checkTheExistence($postId, $postIdList, $msg, $id);
+        if($postIdList == null){
+            DB::table('campaigns')->where('id', $id)->update(['posts'=> array($postId)]);
+        } else {
+            array_push($postIdList, $postId);
+            DB::table('campaigns')->where('id', $id)->update(['posts'=> $postIdList]);
+        }
+
+        header("Location: http://localhost:8000/campaignHandler/$id", true);
+        die();
     }
 }
