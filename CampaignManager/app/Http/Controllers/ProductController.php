8bf43db36f2d1705a54b860a1e41a30015af62ac
Laciface <csomali18@gmail.com>
2021-05-11 14:53:10 +0200

    feature: addProductToCampaign function implemented

--- CampaignManager/app/Http/Controllers/ProductController.php
+++ CampaignManager/app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Campaign;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -27,9 +28,25 @@ class ProductController extends Controller
         $img->stream();
         Storage::disk('local')->put('public/images/'. $fileName, $img);
 
-        $arrayToInsert = array('name'=>$name, 'description' =>$description, 'picture' => $fileName);
+        $arrayToInsert = array('name'=>$name, 'description' => $description, 'picture' => $fileName);
         DB::table('products')->insert($arrayToInsert);
 
         return redirect()->route('displayCampaigns');
+    }
+
+    public function addProductToCampaign(Request $request, $id){
+        $productId = $request->input('productId');
+        $productIdList = Campaign::where('id', $id)->value('products');
+        if($productIdList == null){
+            DB::table('campaigns')->where('id', $id)->update(['products'=> array($productId)]);
+        } else {
+            array_push($productIdList, $productId);
+            DB::table('campaigns')->where('id', $id)->update(['products'=> $productIdList]);
+        }
+
+        /*DB::table('campaigns')->where('id', $id)->update(['products'=> array($productId)]);*/
+
+        header("Location: http://localhost:8000/campaignHandler/$id", true);
+        die();
     }
 }
