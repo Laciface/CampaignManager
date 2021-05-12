7baea81ef456f75b26fdf51313610838db29d10b
Laciface <csomali18@gmail.com>
2021-05-12 08:11:36 +0200

    modify: got styled

--- CampaignManager/resources/views/allCampaign.blade.php
+++ CampaignManager/resources/views/allCampaign.blade.php
@@ -2,12 +2,21 @@
 
 @section('center')
 
-<div>
+<div id="campaignContainer">
     @foreach($campaigns as $campaign)
-        <div>
+        @if($campaign->is_running)
+        <div class="campaignCard active">
             <p>{{$campaign->name}}</p>
+            <p>{{$campaign->first_day}} - {{$campaign->last_day}}</p>
             <a href="/campaignHandler/{{$campaign->id}}">Megtekint</a>
         </div>
+        @else
+            <div class="campaignCard">
+                <p>{{$campaign->name}}</p>
+                <p>{{$campaign->first_day}} - {{$campaign->last_day}}</p>
+                <a href="/campaignHandler/{{$campaign->id}}">Megtekint</a>
+            </div>
+        @endif
     @endforeach
 </div>
 
--- CampaignManager/resources/views/layout/header.blade.php
+++ CampaignManager/resources/views/layout/header.blade.php
@@ -3,16 +3,21 @@
 <head>
     <title>Laci</title>
     <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/header.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
 </head>
 
-<body>
-    <div>
-        <a href="/"><h1>Kampánymenedzser</h1></a>
-    </div>
+<body class="body">
+    <div class="header">
+        <div>
+            <a href="/"><h1>Kampánymenedzser</h1></a>
+        </div>
 
-    <div>
-        <a href="/productForm">Termékek</a>
-        <a href="/blogPostForm">Blog posztok</a>
-        <a href="/couponForm">Kuponok</a>
-        <a href="/campaignForm">Kampányok</a>
+        <div class="links">
+            <a href="/productForm">Termékek</a>
+            <a href="/blogPostForm">Blog posztok</a>
+            <a href="/couponForm">Kuponok</a>
+            <a href="/campaignForm">Kampányok</a>
+        </div>
     </div>
